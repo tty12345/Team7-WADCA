a971ac3e382cb41d761a7c6373eb2b92aadece67
tty12345 <teckyi1993@gmail.com>
2021-06-28 23:47:54 +0800

    email

--- Team7-WDCA(Alvin)/src/main/java/sg/edu/iss/caps/controller/emailController.java
+++ Team7-WDCA(Alvin)/src/main/java/sg/edu/iss/caps/controller/emailController.java
@@ -1,6 +1,5 @@
 package sg.edu.iss.caps.controller;
 
-import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 
@@ -10,18 +9,28 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sg.edu.iss.caps.domain.Student;
+import sg.edu.iss.caps.service.StudentService;
+
 @RestController
 public class emailController {
-   @RequestMapping(value = "/sendemail")
-   public String sendEmail() throws MessagingException {
+	
+	@Autowired
+	private StudentService sservice;
+	
+   @RequestMapping(value = "/sendemail/{id}")
+   public String sendEmail(Model model, @PathVariable("id") Integer id) throws MessagingException {
 	   
+	   Student StudentToEmail = sservice.findStudentById(id);
+	   	   
 	   Properties props = new Properties();
 	   props.put("mail.smtp.auth", "true");
 	   props.put("mail.smtp.starttls.enable", "true");
@@ -39,11 +48,8 @@ public class emailController {
 
 	   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("a0124939w@gmail.com"));
 	   msg.setSubject("Testing");
-	   msg.setContent("Registration Confirmed", "text/html");
+	   msg.setContent(StudentToEmail.getFirstName(), "text/html");
 	   msg.setSentDate(new Date());
-
-	   MimeBodyPart messageBodyPart = new MimeBodyPart();
-	   messageBodyPart.setContent("Tutorials point email", "text/html");
 
 	   Transport.send(msg);   
 	   
